b3e4a34aaeb33354fdc0bc555dcd251033f174d4
sutaC <catus.pl@gmail.com>
2025-05-30 00:39:17 +0200

    Pages structure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <a href="/login">Logout</a>
+        </>
+    );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+            <h1>Login</h1>
+            <a href="/dashboard">Dashboard</a>
+        </>
+    );
+}
