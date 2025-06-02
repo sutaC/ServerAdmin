09b56fc2523feed5d770e26d5f9e38cef8c4846a
sutaC <catus.pl@gmail.com>
2025-06-02 10:18:24 +0200

    Basic styles and login page styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,14 @@
-import { login } from "@/lib/auth";
+import styles from "./page.module.css";
 import LoginForm from "@/lib/components/LoginForm";
+import { login } from "@/lib/auth";
 
 export default function Page() {
     return (
-        <>
-            <h1>Login</h1>
-            <LoginForm action={login} />
-        </>
+        <div className={styles.app}>
+            <main>
+                <h1>Login</h1>
+                <LoginForm action={login} />
+            </main>
+        </div>
     );
 }
--- src/lib/components/LoginForm.tsx
+++ src/lib/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import styles from "./LoginForm.module.css";
 import { login } from "@/lib/auth";
 import { useActionState } from "react";
 
@@ -9,9 +10,11 @@ export default function LoginForm(props: { action: typeof login }) {
 
     return (
         <form action={formAction}>
-            {formState.message && <small>{formState.message}</small>}
+            {formState.message && (
+                <small className={styles.error}>{formState.message}</small>
+            )}
 
-            <div className="field">
+            <div className={styles.field}>
                 <label htmlFor="password">Password:</label>
                 <input
                     type="password"
